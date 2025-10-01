63fc08463a5df5755a83690b311eb40ba8f16509
Artem <you@example.com>
2025-10-01 21:36:16 +0300

    feature-001 added endpoint

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import numpy as np
+
+from predict import analyze_folder
+
+app = FastAPI(title="ML Service", version="1.0")
+
+@app.get("/api/predict/")
+async def get_predict(guid: str):
+    result = analyze_folder(f"/Users/artemsevcenko/dataset/{guid}")
+    return str(np.float32(result))
